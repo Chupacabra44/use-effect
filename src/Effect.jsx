4d186e43db8706fe4889d67f5e4cb6318ee279b5
Sanja Daic <sanja.daic86@gmail.com>
2026-01-05 12:11:17 +0100

    Clean up function

--- src/Effect.jsx
+++ src/Effect.jsx
@@ -3,14 +3,27 @@ import { useState, useEffect } from "react";
 function Effect() {
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    console.log("Mounting + Update");
-    setTimeout(() => {
-      alert("data fetched");
-    }, 1000);
-  }, [count]);
+  // useEffect(() => {
+  //   console.log("Mounting + Update");
+  //   setTimeout(() => {
+  //     alert("data fetched");
+  //   }, 1000);
+  // }, [count]);
 
-  console.log("Render");
+  // console.log("Render");
+
+  const fetchData = () => {
+    console.log("Clean-up");
+  };
+
+  useEffect(() => {
+    console.log("Mounting");
+    const timerId = setInterval(fetchData, 1000);
+    return () => {
+      clearInterval(timerId);
+      console.log("Unmounting");
+    };
+  }, []);
 
   return (
     <div>
